8e0a7bce489dfc146a6051e4ca27a39180e42a88
Matteo <matteonini95@gmail.com>
2020-11-06 22:02:24 +0100

    [FIX] Navbar Mobile Version

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@
 
 let mainNav = document.getElementById("navbar");
 let navBarToggle = document.getElementById("js-navbar-toggle");
+let icon = document.getElementById("landing__nav__toogle__icon");
 
 navBarToggle.addEventListener("click", function () {
+  icon.classList.toggle("fa-times");
   mainNav.classList.toggle("active");
 });
