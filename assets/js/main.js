e74c2c30b89283de2670620fa83ed1e1ea0d0b60
Matteo <matteonini95@gmail.com>
2020-11-07 11:36:11 +0100

    [ISSUE] Video section

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,30 @@ navBarToggle.addEventListener("click", function () {
   icon.classList.toggle("fa-times");
   mainNav.classList.toggle("active");
 });
+
+//Video section controls styling
+var video = document.getElementById("video");
+
+function togglePlayPause() {
+  var playpause = document.getElementById("playpause");
+  var play = "<i class='fas fa-play fa-3x'></i>";
+  var pause = "<i class='fas fa-pause fa-3x'></i>";
+  if (video.paused || video.ended) {
+    playpause.title = "pause";
+    playpause.style.display = "none";
+    video.play();
+  } else {
+    playpause.title = "play";
+    playpause.innerHTML = play;
+    video.pause();
+  }
+}
+/*
+function updateProgress() {
+  var progress = document.getElementById("progress");
+  var value = 0;
+  if (video.currentTime > 0) {
+    value = Math.floor((100 / video.duration) * video.currentTime);
+  }
+  progress.style.width = value + "%";
+}*/
